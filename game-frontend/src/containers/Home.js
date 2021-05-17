4ae4d1dbb6a369bc9284adadbe2b393eed66a071
JohnMcCabe <fredthefurrypotato@hotmail.com>
2021-05-17 15:53:17 -0400

    stage 1 of work

---
+++ game-frontend/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+    return(
+         <div className="Home">
+              <div className="lander">
+                <h1>The Game</h1>
+                <p className="text-muted">A simple app</p>
+              </div>
+         </div>
+    );
+}
